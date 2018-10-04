7fc6b9994a6031c7e966da1a5a7f5b384ff94733
Merrick Sapsford <merrick@sapsford.tech>
2018-10-04 21:46:51 +0200

    Add tests project

---
+++ Sources/RandientTests/RandientTests.swift
@@ -0,0 +1,33 @@
+//
+//  RandientTests.swift
+//  RandientTests
+//
+//  Created by Merrick Sapsford on 04/10/2018.
+//  Copyright © 2018 UI At Six. All rights reserved.
+//
+
+import XCTest
+
+class RandientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
